ce04f903a0dfdeac188e1591a65420534d029f4b
Michael Dippery <michael@monkey-robot.com>
2016-06-12 20:35:10 -0700

    Revert "Completed project files for IB Actions & Outlets lesson"
    Students should begin with a project in a pristine form. The code
    changes were committed to the repo for posterity, in case we want to
    pull them out at some point; recovering them is as easy as reverting
    this commit.
    
    This reverts commit 2c0e62001cb7483c06430d47b4282d451cbbd3f8.

--- IBOutletFun/IBOutletFun/ViewController.swift
+++ IBOutletFun/IBOutletFun/ViewController.swift
@@ -10,26 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var textDisplay: UILabel!
-
-    @IBAction func helloWorld(sender: AnyObject) {
-        print("Pressed Hello World button")
-        textDisplay.text = "Hello, world!"
-    }
-
-    @IBAction func goodbyeWorld(sender: AnyObject) {
-        print("Pressed Goodbye World button")
-        textDisplay.text = "Goodbye, world!"
-    }
-
-    @IBAction func unicorn(sender: AnyObject) {
-        print("Pressed Unicorn! button")
-        textDisplay.text = "🦄"
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        textDisplay.text = "Zzz..."
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
